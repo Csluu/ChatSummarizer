5fbb69adb03223fcc8c3ba74e397541a39fed346
Chris <csluu@protonmail.com>
2023-12-17 19:59:07 -0500

    Initial Files

---
+++ ChatSummarizer/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+	return (
+		<main className="h-screen w-full bg-gray-900 p-5">
+			<h1 className="text-2xl text-white">How can I help you today?</h1>
+
+			<form className="fixed bottom-5 rounded-2xl bg-gray-800 p-5">
+				Click on the Vite and React logos to learn more
+			</form>
+		</main>
+	);
+}
+
+export default App;
